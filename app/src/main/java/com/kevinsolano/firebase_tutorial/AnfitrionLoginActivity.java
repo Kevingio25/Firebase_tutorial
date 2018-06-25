47c59458ee17f426ac0a0ff6f5d4c577562154d8
kevinsolanorodriguez <kevingiovani25@gmail.com>
2018-06-24 19:04:47 -0500

    Firebase Tutorial es un apoyo para la introducción en firebase y android

--- app/src/main/java/com/kevinsolano/firebase_tutorial/AnfitrionLoginActivity.java
+++ app/src/main/java/com/kevinsolano/firebase_tutorial/AnfitrionLoginActivity.java
@@ -24,7 +24,7 @@
 
         import java.util.HashMap;
         import java.util.Map;
-//esta bien
+//el bueno
 public class AnfitrionLoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
